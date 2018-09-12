13dee3f7ff4bad57d447375b763b94da28da27a1
Justin Newby <jnewby91@gmail.com>
2018-09-11 21:35:21 -0400

    Adds changes that fix errors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 "use strict";
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser =  require('body-parser');
 
 //Make Mongoose global with ES6 promises 
 
@@ -12,7 +13,7 @@ const {PORT, DATABASE_URL, TEST_DATABASE_URL} = require("./config");
 const {Blog} = require("./models");
 
 const app = express(); 
-app.use = (express.json());
+app.use(bodyParser.json());
 
 app.get('/posts', (req, res) => {
     Blog
@@ -32,14 +33,18 @@ app.get('/posts', (req, res) => {
 app.get('/posts/:id', (req, res) => {
     Blog
         .findById(req.params.id)
+        .exec()
         .then(blog => res.json(blog.serialize()))
-        .catch(err => console.error(err));
-        res.status(500).json({message: 'Internal server error'});
-})
+        .catch(err => {
+            console.error(err)
+            return res.status(500).json({message: 'Internal server error'});
+        });
+        
+});
 
 app.post('/posts', (req, res) => {
     const requiredFields = ['title', 'author','content'];
-    for(i = 0; i < requiredFields.length; i++){
+    for(let i = 0; i < requiredFields.length; i++){
         const field = requiredFields[i];
         if(!(field in req.body)){
             const message = `Missing\` ${field}\`in the request body`;
@@ -60,39 +65,41 @@ app.post('/posts', (req, res) => {
 
     } )
 
-    app.put('posts/:id', (req,res) => {
-        if(!(req.params.id && req.body.id && req.params.id === req.body.id)){
-            const message = `Request path id ${req.params.id} and request body id` + 
-                `${req.body.id} must match `;
-            console.error(message);
-            return res.status(400).json({message: message}); 
+})
 
-        }
-    })
+app.put('posts/:id', (req, res) => {
+    if(req.params.id  !== req.body.id) {
+        const message = `Request path id ${req.params.id} and request body id 
+            ${req.body.id} must match `;
+        console.error(message);
+        return res.status(400).json({message: message}); 
+    }
 
-    const toUpdate = {};
-    const updateableFields = ['title', 'author', 'content']
 
-    updateableFields.forEach(field => {
-        if (field in req.body){
-            toUpdate[field] = req.body[field];
-        }
-    });
+const toUpdate = {};
+const updateableFields = ['title', 'author', 'content']
 
-    Blog
-    .findByIdAndUpdate(req.params.id, ({$set: toUpdate})
-    .then(blog => res.status(204).end())
-    .catch(err => res.status(500).json({message: "Internal Server Error"}))
-);
+updateableFields.forEach(field => {
+    if (field in req.body){
+        toUpdate[field] = req.body[field];
+    }
+});
+
+Blog
+.findByIdAndUpdate(req.params.id, {$set: toUpdate})
+.then(blog => res.status(204).end())
+.catch(err => res.status(500).json({message: "Internal Server Error"}));
+});
 
 app.delete("/posts/:id", (req,res) => {
-    blog
-    .findByIdandRemove(req.params.id)
+    Blog
+    .findByIdAndRemove(req.params.id)
     .then(blog => res.status(204).end())
     .catch(err => res.status(500).json({message: 'Internal Server Error'}));
 
 })
-});
+
+// });
 
 // app.use("*", function(req,res, next){
 //     next(); 
